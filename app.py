53bc7744be919fccc2120d742ad6ab609e42d3b7
Hridhay <hridhaykrishna2006@gmail.com>
2025-10-05 16:43:55 +0530

    xadens 3rd commit

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] ='static/uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/aboutus.html')
+@app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
 
@@ -16,7 +20,8 @@ def contactus():
 
 @app.route('/galary')
 def galary():
-    return render_template('galary.html')
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('galary.html',images=images)
 
 @app.route('/academics')
 def academics():
